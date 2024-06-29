4ab639bad6e6cc4aaca12222d1d267a0f0089358
amito6 <amitchain24@gmail.com>
2024-06-29 11:28:10 -0400

    routr files

--- index.js
+++ index.js
@@ -1,6 +1,59 @@
 const http = require("http");
+const fs = require("fs");
+
+const route = (response,path,code,type) =>{
+    fs.readFile(path,(error,dataRes)=>{
+        if(error){
+            throw error;
+        }
+        else{
+            response.writeHead(statusCode,{
+                "Content-Type" : type
+            });
+            response.write(dataRes);
+            response.edn();
+        }
+    })
+}
+
+
 const server = http.createServer((request,response)=>{
- response.write("success");
- response.end();
+ if(request.url == "/"  || request.url == "/home"){
+    let path = "html/homepage.html";
+    let code = 200;
+    let type = "text/html";
+    route(response,path,code,type);
+ }
+ else if(request.url == "/about"){
+    let path = "html/about.html";
+    let code = 200;
+    let type = "text/html";
+    route(response,path,code,type);
+ }
+
+ /* code for css */
+ else if(request.url == "/css/homepage.css"){
+    let path = "css/homepage.css";
+    let code = 200;
+    let type = "text/css";
+    route(response,path,code,type);
+ }
+
+ /* code for js */
+ else if(request.url == "/js/homepage.js"){
+    let path = "js/homepage.js";
+    let code = 200;
+    let type = "text/javascript";
+    route(response,path,code,type);
+ }
+ else{
+    let path = "html/not-found.html";
+    let code = 404;
+    let type = "text/html";
+    route(response,path,code,type)
+ }
+ 
 });
+
+
 server.listen(8080);
