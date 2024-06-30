566ddf4189d54528e7199a92c5e5e972a864f88d
amito6 <amitchain24@gmail.com>
2024-06-30 00:00:28 -0400

    toast-functionality

--- js/homepage.js
+++ js/homepage.js
@@ -16,7 +16,23 @@ function ajaxRequest(){
         ajax.open("POST","/api/signup",true);
         ajax.send(formData);
         ajax.onload = function(){
-            console.log(this.response);
+            const data = JSON.parse(this.response);
+            if(data.message.toLowerCase() == "match found"){
+                showMessage("User Already Exists","fa fa-exclamation-circle mx-2","red")
+            }else{
+                showMessage("SignUp Success","fa fa-check-circle mx-2","green");
+                form.reset();
+            }
         }
     }
+}
+
+const showMessage = (msg,icon,color) =>{
+    $(".toast").toast("show");
+    $(".toast-header i").addClass(icon);
+    $(".toast-header").css({
+        color : color
+    });
+    $(".toast").addClass("animate__animated animate__slideInRIght");
+    $(".toast-body p").html(msg)
 }
